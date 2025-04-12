eefc16af44f8df15c6a159e8a67b80cfd66ac2ef
Jyri Hakala <jyri.hakala@tuni.fi>
2025-04-12 16:15:44 +0300

    Disable react-in-jsx-scope rule & format

--- frontend/client/src/types/Student.ts
+++ frontend/client/src/types/Student.ts
@@ -5,24 +5,24 @@ import { Project } from "./Project";
 import { Score } from "./Score";
 
 export type Student = {
-  id: number,
-  name: string,
-  homeUniversity: string,
-  attendingUniversity: string,
-  city: string,
-  cvLink: string,
-  degreeLevelType: string,
-  notes: string[],
-  socialNetworkLinks: Map<string, string>,
-  studiesDescription: string,
-  studiesField: string,
-  studiesType: string,
-  whyGoodCreator: string,
-  whyJoinDemola: string,
-  whyRole: string,
-  applications: ApplicationData[],
-  fakeName?: string,
-  score?: Score
+  id: number;
+  name: string;
+  homeUniversity: string;
+  attendingUniversity: string;
+  city: string;
+  cvLink: string;
+  degreeLevelType: string;
+  notes: string[];
+  socialNetworkLinks: Map<string, string>;
+  studiesDescription: string;
+  studiesField: string;
+  studiesType: string;
+  whyGoodCreator: string;
+  whyJoinDemola: string;
+  whyRole: string;
+  applications: ApplicationData[];
+  fakeName?: string;
+  score?: Score;
 };
 
 export type StudentWithColumn = {
